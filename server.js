cf4edf7dc18a6ed1a95d5e2ed7c496ae593d2ab1
unknown <i.kewa@alustudent.com>
2025-10-07 21:59:39 +0200

    connected the backend to the frontemd

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import protectedRoutes from "./routes/protectedRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/users", userRoutes);
